f9f8a578b2d2e6313315a7812d4f28ff2330f0b7
Mr-NMishra <mishra.narendra003@gmail.com>
2022-01-16 00:59:31 +0530

    First Spring boot app Demo

---
+++ FirstSpring-16-01/src/main/java/com/mishra/FirstSpring1601Application.java
@@ -0,0 +1,13 @@
+package com.mishra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpring1601Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpring1601Application.class, args);
+	}
+
+}
---
+++ FirstSpring-16-01/src/test/java/com/mishra/FirstSpring1601ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mishra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpring1601ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
