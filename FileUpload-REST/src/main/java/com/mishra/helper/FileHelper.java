444336962b1ca9fd8c5535a54f531ca56b58076c
Mr-NMishra <mishra.narendra003@gmail.com>
2022-01-19 03:50:53 +0530

    REST-MULTIPARTFILE--FILEUPLOAD

--- FileUpload-REST/src/main/java/com/mishra/helper/FileHelper.java
+++ FileUpload-REST/src/main/java/com/mishra/helper/FileHelper.java
@@ -2,7 +2,10 @@ package com.mishra.helper;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.InputStream;import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,17 +21,22 @@ public class FileHelper {
 	public boolean uploadFie(MultipartFile file) {
 		boolean flag=false;
 		try {
-			//here we are reading the data
-			InputStream inputStream= file.getInputStream();
-			byte[] bytes= file.getBytes();
-			inputStream.read(bytes);
-			System.out.println(inputStream);
+						/* here we are reading the data */
 			
-			//now writing the data into folder
-			FileOutputStream outputStream= new FileOutputStream(DIR_UPLD+File.separator+file.getOriginalFilename());
-			outputStream.write(bytes);
-			outputStream.close();
-			inputStream.close();
+//			InputStream inputStream= file.getInputStream();
+//			byte[] bytes= file.getBytes();
+//			inputStream.read(bytes);
+//			System.out.println(inputStream);
+//			
+//			//now writing the data into folder
+//			FileOutputStream outputStream= new FileOutputStream(DIR_UPLD+File.separator+file.getOriginalFilename());
+//			outputStream.write(bytes);
+//			outputStream.close();
+//			inputStream.close();
+			
+						/* OR WE CAN ALSO USE POWERFULL NIO FEATURE */
+			Files.copy(file.getInputStream(),Paths.get(DIR_UPLD+File.separator+file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+			
 			flag=true;
 			return flag;
 			
