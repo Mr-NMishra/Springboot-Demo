3d8ba9f6af5597e51e35dd2aa34c0976d4896c0b
Mr-NMishra <mishra.narendra003@gmail.com>
2022-01-18 20:48:51 +0530

    RESTFUL--HttpResponse

--- RestAPI-SpringBoot/src/main/java/com/mishra/controller/BookCltr.java
+++ RestAPI-SpringBoot/src/main/java/com/mishra/controller/BookCltr.java
@@ -1,9 +1,11 @@
 package com.mishra.controller;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,8 +25,21 @@ public class BookCltr {
 	
 	//Getting list of all Books
 	@GetMapping("/books")
-	public List<Book> Test() {
-		return bookService.findAllBook();
+	public ResponseEntity<List<Book>>  Test() {
+		List<Book> list=bookService.findAllBook();
+		//checking list is empty or not
+		if(list.isEmpty()) {
+			//using status Static method of Response class
+			//and passing HttpStatus
+			//here build method is used to creae response object
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			
+		}else {
+			//if list having elements then we should
+			//pass response with body i.e. elements
+			return ResponseEntity.of(Optional.of(list));
+		}
+		//bookService.findAllBook();
 	}
 	
 	//Getting books using Id
