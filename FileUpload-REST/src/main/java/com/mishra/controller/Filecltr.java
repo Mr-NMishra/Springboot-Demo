8c3b70e43225b373cf36e37b1b47dbb6985de2f7
Mr-NMishra <mishra.narendra003@gmail.com>
2022-01-19 14:34:29 +0530

    REST-URI-RESPONSE--FILEUPLOAD

--- FileUpload-REST/src/main/java/com/mishra/controller/Filecltr.java
+++ FileUpload-REST/src/main/java/com/mishra/controller/Filecltr.java
@@ -1,5 +1,7 @@
 package com.mishra.controller;
 
+import java.io.File;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,24 +9,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mishra.helper.FileHelper;
 
 @RestController
 public class Filecltr {
 	@Autowired
-	private FileHelper fileHelper;
+	private FileHelper fileHelper ;
 
 	@PostMapping("upload-file")
-	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile multipartFile) {
-		System.out.println(multipartFile.getContentType());
+	public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile multipartFile) {
 		if (multipartFile.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("File Not Found");
 		}
 		if (!multipartFile.getContentType().equals("image/png")) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid File Type");
 		}
-		fileHelper.uploadFie(multipartFile);
-		return ResponseEntity.ok("Test Sucessfull");
-	}
+		boolean flag=fileHelper.uploadFie(multipartFile);
+		//This returns only Text 
+		//return ResponseEntity.ok("Test Sucessfull");
+		//Now we sending absolute directory
+		if (flag) {
+			return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/upload-file/").path(multipartFile.getOriginalFilename()).toUriString());
+
+		} else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+			}
 }
--- FileUpload-REST/src/main/java/com/mishra/helper/FileHelper.java
+++ FileUpload-REST/src/main/java/com/mishra/helper/FileHelper.java
@@ -2,44 +2,61 @@ package com.mishra.helper;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.InputStream;import java.nio.file.CopyOption;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileHelper {
-	
-	//adding final folder
-	private final String DIR_UPLD="/home/nick/Desktop/Git-Repos/Springboot-11-01-22/FileUpload-REST"
-			+ "/src/main/resources/static/uploads/Images";
 
-	//uploading file
+	/*
+	 * adding final folder this is static means all folders are reuired in same
+	 * order
+	 */
+
+//	private final String DIR_UPLD="/home/nick/Desktop/Git-Repos/Springboot-11-01-22/FileUpload-REST"
+//			+ "/src/main/resources/static/uploads/Images";
+
+	/* Now Creating Dyanmic path so our project Directory is dependent not other */
+	private final String DIR_UPLD = new ClassPathResource("static/uploads/Images").getFile().getAbsolutePath();
+	//Default constructor to handel Excpetion throws by final path 
+	FileHelper() throws IOException {
+
+	}
+
+	// uploading file
 	public boolean uploadFie(MultipartFile file) {
-		boolean flag=false;
+		
+		boolean flag = false;
 		try {
-						/* here we are reading the data */
-			
-//			InputStream inputStream= file.getInputStream();
-//			byte[] bytes= file.getBytes();
-//			inputStream.read(bytes);
-//			System.out.println(inputStream);
-//			
-//			//now writing the data into folder
-//			FileOutputStream outputStream= new FileOutputStream(DIR_UPLD+File.separator+file.getOriginalFilename());
-//			outputStream.write(bytes);
-//			outputStream.close();
-//			inputStream.close();
-			
-						/* OR WE CAN ALSO USE POWERFULL NIO FEATURE */
-			Files.copy(file.getInputStream(),Paths.get(DIR_UPLD+File.separator+file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
-			
-			flag=true;
+			/* here we are reading the data */
+			InputStream inputStream = file.getInputStream();
+			byte[] bytes = file.getBytes();
+			inputStream.read(bytes);
+			// now writing the data into folder
+			// DIR_UPLD is qualified folder name in string formate
+			FileOutputStream outputStream = new FileOutputStream(
+					DIR_UPLD + File.separator + file.getOriginalFilename());
+			outputStream.write(bytes);
+			outputStream.close();
+			inputStream.close();
+
+			/* OR WE CAN ALSO USE POWERFULL NIO FEATURE */
+			// here we use Paths to convert our destination from String to Path
+
+			Files.copy(file.getInputStream(), Paths.get(DIR_UPLD + File.separator + file.getOriginalFilename()),
+					StandardCopyOption.REPLACE_EXISTING);
+
+			flag = true;
 			return flag;
-			
+
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
